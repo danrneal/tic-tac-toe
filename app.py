13824d05c0270c1df497ca100484419b6935b8af
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-26 12:15:30 -0700

    Add abiliity for computer to make moves

--- app.py
+++ app.py
@@ -14,35 +14,29 @@ MOVES = []
 def index():
 
     if "board" not in session:
-        new_board()
+        return redirect(url_for("reset"))
 
     return render_template(
         "index.html",
         winner=session["winner"],
+        tie=session["tie"],
         game=session["board"],
-        turn=session["turn"],
+        player=session["player"],
+        new_game=session["new_game"]
     )
 
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
-    session["board"][row][col] = session["turn"]
-
-    if (
-        all(move == session["turn"] for move in session["board"][row]) or
-        all(move[col] == session["turn"] for move in session["board"]) or
-        (
-            row == col and
-            all(session["board"][i][i] == session["turn"] for i in range(3))
-        ) or 
-        (
-            row + col == 2 and
-            all(session["board"][i][2-i] == session["turn"] for i in range(3))
-        )
-    ):
-        session["winner"] = session["turn"]
-
-    switch_player()
+    session["board"][row][col] = session["player"]
+    session["new_game"] = False
+    session["winner"] = is_winner(
+        session["board"],
+        session["player"],
+        (row, col)
+    )
+    session["tie"] = is_tie(session["board"])
+    session["player"] = switch_player(session["player"])
     MOVES.append((row, col))
 
     return redirect(url_for("index"))
@@ -50,33 +44,98 @@ def play(row, col):
 
 @app.route("/reset")
 def reset():
-    new_board()
-    return redirect(url_for("index"))
-
-
-@app.route("/undo")
-def undo():
-    if len(MOVES) > 0:
-        (row, col) = MOVES.pop()
-        session["winner"] = None
-        session["board"][row][col] = None
-        switch_player()
-
-    return redirect(url_for("index"))
-
-
-def new_board():
     session["winner"] = None
+    session["tie"] = False
     session["board"] = [
         [None, None, None],
         [None, None, None],
         [None, None, None],
     ]
-    session["turn"] = "X"
+    session["player"] = "X"
+    session["new_game"] = True
+
+    return redirect(url_for("index"))
 
 
-def switch_player():
-    if session["turn"] == "X":
-        session["turn"] = "0"
+@app.route("/undo")
+def undo():
+
+    if len(MOVES) > 0:
+        (row, col) = MOVES.pop()
+        session["winner"] = None
+        session["tie"] = False
+        session["board"][row][col] = None
+        session["player"] = switch_player(session["player"])
+
+    if len(MOVES) == 0:
+        session["new_game"] = True
+
+    return redirect(url_for("index"))
+
+
+@app.route("/computer_move")
+def computer_move():
+    row, col = minimax(session["board"], switch_player(session["player"]))[0]
+    return redirect(url_for("play", row=row, col=col))
+
+
+def switch_player(player):
+    if player == "X":
+        return "O"
     else:
-        session["turn"] = "X"
+        return "X"
+
+
+def is_winner(board, player, move):
+    row, col = move
+    if (
+        all(square == player for square in board[row]) or
+        all(row[col] == player for row in board) or
+        (row == col and all(board[i][i] == player for i in range(3))) or
+        (row + col == 2 and all(board[i][2-i] == player for i in range(3)))
+    ):
+        return player
+
+
+def is_tie(board):
+
+    for row in board:
+        if None in row:
+            return False
+
+    return True
+
+
+def minimax(board, player, move=None, depth=0):
+
+    if move is not None and is_winner(board, player, move) == "O":
+        return (move, 1 / depth)
+
+    if move is not None and is_winner(board, player, move) == "X":
+        return (move, -1 / depth)
+
+    if is_tie(board):
+        return (move, 0)
+
+    player = switch_player(player)
+
+    if player == "O":
+        value = -1
+    else:
+        value = 1
+
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] is None:
+                new_board = [row.copy() for row in board]
+                new_board[row][col] = player
+                new_value = minimax(new_board, player, (row, col), depth+1)[1]
+
+                if player == "O" and new_value >= value:
+                    value = new_value
+                    move = (row, col)
+                elif player == "X" and new_value <= value:
+                    value = new_value
+                    move = (row, col)
+
+    return (move, value)
