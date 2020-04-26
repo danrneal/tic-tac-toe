5df0a70190fab206fd32d6e5e33325530cc91ae6
Daniel R. Neal <dan.r.neal@gmail.com>
2020-04-25 17:19:04 -0700

    Remove tmp dir so that sessions persist on Heroku

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
-from tempfile import mkdtemp
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
